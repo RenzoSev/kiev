e43ed6da42430dcf7742bef6e7eb992a8b9310a2
Renzo Sevilha <sevilharenzo@gmail.com>
2022-03-02 18:50:29 -0300

    feat(front): creates header component

--- packages/front/src/App.tsx
+++ packages/front/src/App.tsx
@@ -1,8 +1,10 @@
+import Header from './components/Header';
+
 function App() {
   return (
-    <div>
-      <h1 className="text-lg text-teal-400">Kiev.Daily</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
---
+++ packages/front/src/components/Header.tsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header className="p-3.5 border-b border-gray-700">
+      <div>
+        <h1 className="text-xl">
+          <span className="text-amber-400">Kiev</span>
+          <span className="text-neutral-200">.Daily</span>
+        </h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
